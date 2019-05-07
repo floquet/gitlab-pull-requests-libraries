a431d87cd1bc68cc509c9c1e2a10db428b5e261a
Daniel Topa <dantopa@lanl.gov>
2019-05-07 11:52:42 -0700

    fix modified:   libraries/tools/bash/update-rzansel-spack-libraries.sh

--- libraries/tools/bash/update-rzansel-spack-libraries.sh
+++ libraries/tools/bash/update-rzansel-spack-libraries.sh
@@ -3,9 +3,9 @@ printf '%s\n' "$(date) $(tput bold)${BASH_SOURCE[0]}$(tput sgr0)"
 
 myTPLs="libhio openmpi pmix ucx"
 
-function update_spack{
+function update_spack() {
     golibraries
-    cd rzansel-power9.${1}
+    cd "rzansel-power9.${1}"
     git stash
     git pull
     cp ${github}/yaml-library/clone-zone/rzansel/*.yaml etc/spack/defaults/.
